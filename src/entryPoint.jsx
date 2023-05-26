b33a977e951bab42e7e028611b34b02988f988c4
Anfelesan <anfelesan@gmail.com>
2023-05-25 23:39:38 -0500

    normalize.css location was changed

--- src/entryPoint.jsx
+++ src/entryPoint.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Routes';
 import './index.css';
+import './styles/normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
