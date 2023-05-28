26629d1717998c4a6b2c9ce274ec8dde6a2680ab
Anfelesan <anfelesan@gmail.com>
2023-05-28 18:52:47 -0500

    Matter card composition was refactored

--- src/components/CurriculumContent/CurriculumContent.jsx
+++ src/components/CurriculumContent/CurriculumContent.jsx
@@ -1,4 +1,4 @@
-import MatterCard from '../MatterCard/MatterCard'
+import MCwithRequirements from '../MatterCard/MCwithRequirements'
 import './CurriculumContent.css'
 
 const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
@@ -12,8 +12,18 @@ const CurriculumContent = ({ content, handleModal }) => {
           <div className='semester-matters-container'>
             {semesterContent.map((e, subIndex) => {
               return typeof (e) === 'number'
-                ? <MatterCard id={e} handleModal={handleModal} key={subIndex + 'matter'} />
-                : <MatterCard {...e} handleModal={handleModal} key={subIndex + 'matter'} />
+                ? <MCwithRequirements id={e} handleModal={handleModal} key={subIndex + 'matter'} />
+                : <MCwithRequirements
+                    id={e.id}
+                    credits={e.credits}
+                    synchronous={e.synchronous}
+                    asynchronous={e.asynchronous}
+                    name={e.name}
+                    group={e.group}
+                    component={e.component}
+                    handleModal={handleModal}
+                    key={subIndex + 'matter'}
+                  />
             })}
           </div>
         </div>
--- src/components/MatterCard/MatterCard.jsx
+++ src/components/MatterCard/MatterCard.jsx
@@ -16,36 +16,21 @@ const MatterCard = ({ id, credits, synchronous, asynchronous, name, group, compo
   })
 
   return (
-    <div className='matterCard-container'>
-      <div className='matterCard-prerequisites-container'>
-        <div className='matterCard-prerequisite-square'>11</div>
-        <div className='matterCard-prerequisite-square'>12</div>
-        <div className='matterCard-prerequisite-square'>13</div>
-        <div className='matterCard-prerequisite-square'>14</div>
-        <div className='matterCard-prerequisite-square'>15</div>
+    <div className='matterCard'>
+      <div className='matterCard-firstData-container'>
+        <span className='matterCard-credits'>{matterInfo.credits}</span>
+        <span className='matterCard-synchronous'>{matterInfo.synchronous}</span>
+        <span>{matterInfo.asynchronous}</span>
       </div>
-      <div className='matterCard'>
-        <div className='matterCard-firstData-container'>
-          <span className='matterCard-credits'>{matterInfo.credits}</span>
-          <span className='matterCard-synchronous'>{matterInfo.synchronous}</span>
-          <span>{matterInfo.asynchronous}</span>
-        </div>
-        <h3 className='matterCard-name'>{matterInfo.name}</h3>
-        <p className='matterCard-group' style={{ backgroundColor: matterInfo.group?.color }}>
-          {matterInfo.group?.name}
-        </p>
-        <div className='matterCard-idComponent-container' style={{ backgroundColor: matterInfo.component?.color }}>
-          <span className='matterCard-id'>{id}</span>
-          <span className='matterCard-component'>{matterInfo.component?.symbol}</span>
-        </div>
-        {id && <button className='matterCard-seeMore' onClick={() => handleModal(id)}>Ver más</button>}
+      <h3 className='matterCard-name'>{matterInfo.name}</h3>
+      <p className='matterCard-group' style={{ backgroundColor: matterInfo.group?.color }}>
+        {matterInfo.group?.name}
+      </p>
+      <div className='matterCard-idComponent-container' style={{ backgroundColor: matterInfo.component?.color }}>
+        <span className='matterCard-id'>{id}</span>
+        <span className='matterCard-component'>{matterInfo.component?.symbol}</span>
       </div>
-      <div className='matterCard-nextMatters-container'>
-        <div className='matterCard-nextMatter-square'>20</div>
-        <div className='matterCard-nextMatter-square'>21</div>
-        <div className='matterCard-nextMatter-square'>22</div>
-        <div className='matterCard-nextMatter-square'>23</div>
-      </div>
+      {id && <button className='matterCard-seeMore' onClick={() => handleModal(id)}>Ver más</button>}
     </div>
   )
 }
